761d3ba4b3c2564a380270eb4e038d0b3cc40fee
crisoalmoniga <cristian.almoniga@gmail.com>
2023-10-22 22:31:10 -0300

    Fondo

---
+++ Fondo/Fondo/Fondo.cpp
@@ -0,0 +1,31 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Fondo de Videojuego");
+
+	// Cargar la textura del fondo, siempre actualizar desde la ruta específica
+	sf::Texture fondoTexture;
+
+	if (!fondoTexture.loadFromFile("D:\\Universidad - Cris\\Videojuegos\\Universidad del Litoral\\MAVI\\Practico_Unidad_3\\Unidad3_Assets\\Unidad3_Assets\\fondo.jpg")) {
+		return 1; // Si la ruta falla, tira error 1.
+	}
+
+	// Crear un sprite para el fondo
+	sf::Sprite fondoSprite(fondoTexture);
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		window.clear();
+
+		// Dibujar el fondo en toda la ventana
+		window.draw(fondoSprite);
+		window.display();
+	}
+	return 0;
+}
